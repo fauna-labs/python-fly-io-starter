173adc81c9d86a6759ffd78fc8647c6a227b7fbd
Zee Khoo <zeekhoo@gmail.com>
2023-07-27 22:31:26 -0700

    cleanup unused imports

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ import os, json
 from fauna import fql
 from fauna.client import Client
 from fauna.encoding import QuerySuccess
-from fauna.errors import QueryRuntimeError, AuthorizationError
 import utils
 
 
